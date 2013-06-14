20bbb977f2534240448c5d6873a12bdec6efe88a
prathima <prathimavr22@gmail.com>
2013-06-14 09:45:49 +0530

    first commit

---
+++ max.py
@@ -0,0 +1,12 @@
+print "Enter three numbers"
+first=int(raw_input("Enter first no:"))
+sec=int(raw_input("Enter second no:"))
+third=int(raw_input("Enter third no:"))
+max=first
+if sec>max:
+    max=sec
+if third>max:
+        max=third
+print "Maximum of three numbers are %d" %max
+print "abcd"
+        
